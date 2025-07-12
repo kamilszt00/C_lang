1762436d6d14f2ce0d5964711a71449402da4199
kamilxcv <kamil.sz00@proton.me>
2025-07-12 15:13:23 +0200

    Variables and stuff

---
+++ main.c
@@ -0,0 +1,11 @@
+#include<stdio.h>
+// Variables and standards of naming them
+// no numbers at first, and no spaces -> use underscores, also no special characters like ! and ?
+// also no keywords as name of the variable
+int main() {
+    int number;
+    int test_value;
+    int example;
+    float int_score;
+    printf("Hello World!\n");
+}
